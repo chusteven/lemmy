fdc34b21786cf2e25a20c8e661149387b391e7ed
Felix Ableitner <me@nutomic.com>
2022-04-11 16:00:40 +0200

    Breaking: change type of Instance to `Application` (ref #2200)

--- crates/apub/src/protocol/objects/instance.rs
+++ crates/apub/src/protocol/objects/instance.rs
@@ -2,24 +2,19 @@ use crate::{
   objects::instance::ApubSite,
   protocol::{ImageObject, Source},
 };
+use activitystreams_kinds::actor::ApplicationType;
 use chrono::{DateTime, FixedOffset};
 use lemmy_apub_lib::{object_id::ObjectId, signatures::PublicKey, values::MediaTypeHtml};
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 use url::Url;
 
-#[derive(Clone, Copy, Debug, Deserialize, Serialize, PartialEq)]
-pub enum InstanceType {
-  Application,
-  Service,
-}
-
 #[skip_serializing_none]
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Instance {
   #[serde(rename = "type")]
-  pub(crate) kind: InstanceType,
+  pub(crate) kind: ApplicationType,
   pub(crate) id: ObjectId<ApubSite>,
   // site name
   pub(crate) name: String,
